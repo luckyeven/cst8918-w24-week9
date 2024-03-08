b16ef947f1f41d4a7a895190f3c897ee478aad72
shifeng <a245241964@gmail.com>
2024-03-08 17:13:21 -0500

    husky commit test

---
+++ infrastructure/variables.tf
@@ -0,0 +1,10 @@
+# Define config variables
+variable "labelPrefix" {
+  type        = string
+ 
+  description = "Your college username. This will form the beginning of various resource names."
+}
+
+variable "region" {
+  default = "canadacentral"
+}
